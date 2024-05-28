90d036c66d0444182bbbf8284b907b9ecc6d4798
arlannunesdev <arlan1.soares1@gmail.com>
2024-05-28 11:24:34 -0300

    Refatoração Serviço: Usuario.
    Co-authored-by: Glaydon Lima <Glaydonlima@users.noreply.github.com>

--- EventusHub2.0/src/main/java/com/eventshub/backend/servico/SolicitacaoServico.java
+++ EventusHub2.0/src/main/java/com/eventshub/backend/servico/SolicitacaoServico.java
@@ -2,11 +2,9 @@ package com.eventshub.backend.servico;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
 import com.eventshub.backend.modelo.ClienteModelo;
 import com.eventshub.backend.modelo.ServicoModelo;
 import com.eventshub.backend.modelo.SolicitacaoModelo;
@@ -14,7 +12,6 @@ import com.eventshub.backend.repositorio.ClienteRepositorio;
 import com.eventshub.backend.repositorio.ServicoRepositorio;
 import com.eventshub.backend.repositorio.SolicitacaoRepositorio;
 import com.eventshub.backend.servico.seguranca.TokenServico;
-
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 
@@ -31,20 +28,28 @@ public class SolicitacaoServico {
 
     private final ServicoRepositorio servicoRepositorio;
 
-    public ResponseEntity<?> cadastrarSolicitacao(SolicitacaoModelo solicitacao, Long idServico, HttpServletRequest request) {
-        try{
-        ServicoModelo servico = servicoRepositorio.findById(idServico).orElseThrow(() -> new RuntimeException("Servico não encontrado"));
-        Long idCliente = tokenServico.extrairIdUsuarioDoToken(tokenServico.recuperarToken(request));
-        ClienteModelo cliente = clienteRepositorio.findById(idCliente)
-          .orElseThrow(() -> new RuntimeException("Cliente não encontrado"));
-          solicitacao.setCliente(cliente);
-          solicitacao.setDataCriacao(LocalDateTime.now());
-          solicitacao.setDataAtualizacao(LocalDateTime.now());
-          solicitacao.setServico(servico);
-          return new ResponseEntity<>(solicitacaoRepositorio.save(solicitacao), HttpStatus.CREATED);
-
-          } catch(Exception e){return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-          }
+    public ResponseEntity<?> cadastrarSolicitacao(final SolicitacaoModelo solicitacao, final Long idServico, final HttpServletRequest request) { 
+        try {
+            final Optional<ServicoModelo> optionalServico = servicoRepositorio.findById(idServico);
+            if (!optionalServico.isPresent()) {
+                return new ResponseEntity<>("Servico não encontrado", HttpStatus.NOT_FOUND);
+            }
+    
+            final Long idCliente = tokenServico.extrairIdUsuarioDoToken(tokenServico.recuperarToken(request));
+            final Optional<ClienteModelo> optionalCliente = clienteRepositorio.findById(idCliente);
+            if (!optionalCliente.isPresent()) {
+                return new ResponseEntity<>("Cliente não encontrado", HttpStatus.NOT_FOUND);
+            }
+    
+            solicitacao.setCliente(optionalCliente.get());
+            solicitacao.setDataCriacao(LocalDateTime.now());
+            solicitacao.setDataAtualizacao(LocalDateTime.now());
+            solicitacao.setServico(optionalServico.get());
+    
+            return new ResponseEntity<>(solicitacaoRepositorio.save(solicitacao), HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     public SolicitacaoModelo atualizarSolicitacao(SolicitacaoModelo solicitacao) {
--- EventusHub2.0/src/main/java/com/eventshub/backend/servico/UsuarioServico.java
+++ EventusHub2.0/src/main/java/com/eventshub/backend/servico/UsuarioServico.java
@@ -5,16 +5,15 @@ import java.util.Optional;
 import java.util.Set;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
 import com.eventshub.backend.dto.LoginRequestDTO;
 import com.eventshub.backend.dto.ResponseDTO;
 import com.eventshub.backend.modelo.RespostaModelo;
 import com.eventshub.backend.modelo.UsuarioModelo;
 import com.eventshub.backend.repositorio.UsuarioRepositorio;
 import com.eventshub.backend.servico.seguranca.TokenServico;
-
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 
@@ -24,73 +23,75 @@ public class UsuarioServico {
 
   private final UsuarioRepositorio usuarioRepositorio;
 
-  private final RespostaModelo respostaModelo;
-
   private final PasswordEncoder passwordEncoder;
 
   private final TokenServico tokenServico;
   
-  public ResponseEntity<?> login(LoginRequestDTO usuarioDto) {
-    UsuarioModelo usuario = this.usuarioRepositorio.findByEmail(usuarioDto.email())
-        .orElseThrow(() -> new RuntimeException("Usuario não encontrado"));
+  public ResponseEntity<?> login(final LoginRequestDTO usuarioDto) {
+    final UsuarioModelo usuario = usuarioRepositorio.findByEmail(usuarioDto.email())
+        .orElseThrow(() -> new UsernameNotFoundException("Usuario não encontrado"));
+
     if (passwordEncoder.matches(usuarioDto.senha(), usuario.getSenha())) {
-      String token = this.tokenServico.geradorToken(usuario);
-      return ResponseEntity.ok(new ResponseDTO(usuario.getNome(), token));
+        final String token = tokenServico.geradorToken(usuario);
+        return ResponseEntity.ok(new ResponseDTO(usuario.getNome(), token));
     }
+
     return ResponseEntity.badRequest().build();
+}
+
+public ResponseEntity<?> cadastrar(final UsuarioModelo usuarioModelo) {
+  if (usuarioRepositorio.existsByEmail(usuarioModelo.getEmail())) {
+      return new ResponseEntity<>("O email já está cadastrado!", HttpStatus.BAD_REQUEST);
   }
 
-  public ResponseEntity<?> cadastrar(UsuarioModelo usuarioModelo) { 
-    if (usuarioRepositorio.existsByEmail(usuarioModelo.getEmail())) {
-      respostaModelo.setMensagem("O email já está cadastrado!");
-      return new ResponseEntity<RespostaModelo>(respostaModelo, HttpStatus.BAD_REQUEST);
+  usuarioModelo.setSenha(passwordEncoder.encode(usuarioModelo.getSenha()));
+  Set<String> roles = new HashSet<>();
+  roles.add("ROLE_USER");
+  usuarioModelo.setRoles(roles);
+  String token = tokenServico.geradorToken(usuarioModelo);
+  usuarioRepositorio.save(usuarioModelo);
+
+  return new ResponseEntity<>(new ResponseDTO(usuarioModelo.getNome(), token), HttpStatus.CREATED);
+}
+
+       public ResponseEntity<?> Alterar(final UsuarioModelo usuarioModelo, final HttpServletRequest request) {
+    final Long idUsuario = tokenServico.extrairIdUsuarioDoToken(tokenServico.recuperarToken(request));
+    final Optional<UsuarioModelo> optionalUsuario = usuarioRepositorio.findById(idUsuario);
+
+    if (!optionalUsuario.isPresent()) {
+        return new ResponseEntity<>("Usuario não encontrado", HttpStatus.NOT_FOUND);
     }
-        usuarioModelo.setSenha(passwordEncoder.encode(usuarioModelo.getSenha()));
-        Set<String> roles = new HashSet<>();
-        roles.add("ROLE_USER");
-        usuarioModelo.setRoles(roles);
-        String token = tokenServico.geradorToken(usuarioModelo);
-        usuarioRepositorio.save(usuarioModelo);
-        return new ResponseEntity<>(new ResponseDTO(usuarioModelo.getNome(), token),
-            HttpStatus.CREATED);
-    } 
 
-       public ResponseEntity<?> Alterar(UsuarioModelo usuarioModelo, HttpServletRequest request){
-        Long idUsuario = tokenServico.extrairIdUsuarioDoToken(tokenServico.recuperarToken(request));
-      Optional<UsuarioModelo> usuarioExistente = usuarioRepositorio.findById(idUsuario);
-      if (usuarioExistente.isPresent()) {
-        UsuarioModelo usuarioExistenteAtualizado = usuarioExistente.get();
-        if (usuarioModelo.getNome() != null && !usuarioModelo.getNome().isEmpty()) {
-          usuarioExistenteAtualizado.setNome(usuarioModelo.getNome());
-        }
-        if (usuarioModelo.getEmail() != null && !usuarioModelo.getEmail().isEmpty()) {
-          usuarioExistenteAtualizado.setEmail(usuarioModelo.getEmail());
-        }
-        if (usuarioModelo.getSenha() != null && !usuarioModelo.getSenha().isEmpty()) {
-          String senha = usuarioModelo.getSenha();
-          usuarioExistenteAtualizado.setSenha(passwordEncoder.encode(senha));
-        }
+    final UsuarioModelo usuarioExistente = optionalUsuario.get();
+    if (usuarioModelo.getNome() != null && !usuarioModelo.getNome().isEmpty()) {
+        usuarioExistente.setNome(usuarioModelo.getNome());
+    }
+    if (usuarioModelo.getEmail() != null && !usuarioModelo.getEmail().isEmpty()) {
         if (usuarioRepositorio.existsByEmail(usuarioModelo.getEmail())) {
-          respostaModelo.setMensagem("O email já está cadastrado!");
-          return new ResponseEntity<RespostaModelo>(respostaModelo, HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>("O email já está cadastrado!", HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity<UsuarioModelo>(
-            usuarioRepositorio.save(usuarioExistenteAtualizado), HttpStatus.OK);
-      } else {
-        return new ResponseEntity<RespostaModelo>(respostaModelo, HttpStatus.NOT_FOUND);
-      }
-  }
+        usuarioExistente.setEmail(usuarioModelo.getEmail());
+    }
+    if (usuarioModelo.getSenha() != null && !usuarioModelo.getSenha().isEmpty()) {
+        usuarioExistente.setSenha(passwordEncoder.encode(usuarioModelo.getSenha()));
+    }
 
-  public ResponseEntity<RespostaModelo> remover(long id) {
-    if (usuarioRepositorio.existsById(id)) {
-      usuarioRepositorio.deleteById(id);
+    return new ResponseEntity<>(usuarioRepositorio.save(usuarioExistente), HttpStatus.OK);
+}
+
+public ResponseEntity<RespostaModelo> remover(final long id) {
+  final Optional<UsuarioModelo> optionalUsuario = usuarioRepositorio.findById(id);
+  if (optionalUsuario.isPresent()) {
+      usuarioRepositorio.delete(optionalUsuario.get());
+      final RespostaModelo respostaModelo = new RespostaModelo();
       respostaModelo.setMensagem("O usuario foi removido com sucesso");
       return new ResponseEntity<>(respostaModelo, HttpStatus.OK);
-    } else {
+  } else {
+      final RespostaModelo respostaModelo = new RespostaModelo();
       respostaModelo.setMensagem("Usuario não encontrado");
       return new ResponseEntity<>(respostaModelo, HttpStatus.NOT_FOUND);
-    }
   }
+}
 
   public Iterable<UsuarioModelo> listar() {
     return usuarioRepositorio.findAll();
